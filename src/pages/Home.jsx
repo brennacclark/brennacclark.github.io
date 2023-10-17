9da3b26c190f46719f122be549169683d473e42b
Brenna Clark <brennacclark@gmail.com>
2023-10-16 22:49:50 -0400

    Splitting up features & styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,106 +1,35 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import '../styles/Home.css';  // Importing the CSS file
 
 const Home = () => {
-  const heroContainerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    background: 'linear-gradient(to right, #FFB778 0%, #8B4513 100%)',
-    color: 'white',
-    textAlign: 'center',
-    position: 'relative',
-    padding: '0 15vw',
-  };
-
-  const profileImageStyle = {
-    width: '40vw',
-    aspectRatio: '1 / 1',
-    backgroundImage: 'url(/BrennaLogo.png)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    zIndex: 2,
-  };
-
-  const profileCircleStyle = {
-    width: '35vw',
-    aspectRatio: '1 / 1',
-    borderRadius: '50%',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    border: '4px solid #FFD700',
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    zIndex: 1,
-  };
-
-  const textSectionStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    height: '50%',
-    padding: '0 20px',
-  };
-
-  const buttonContainerStyle = {
-    margin: '10px',
-    padding: '10px',
-  };
-
-  const buttonStyle = {
-    backgroundColor: '#333',
-    border: '1px solid #333',
-    padding: '1vh 2vw',    // Vertical and horizontal padding for responsiveness
-    margin: '10px',
-    fontSize: '2vh',      // Adjust the font size for responsiveness
-    display: 'inline-block',  // This will allow the button to grow with its content
-    transition: '0.3s',   // Optional: smooth transition for any changes (e.g., hover effects)
-    minWidth: '80px',     // Minimum width to ensure it doesn't get too small
-    minHeight: '30px',    // Minimum height
-    textAlign: 'center',  // Ensure text is centered
-    whiteSpace: 'nowrap', // Ensure text doesn't wrap to the next line
-};
-
-
-  const imageContainerStyle = {
-    position: 'relative',
-    width: '500px',
-    aspectRatio: '1 / 1',
-};
-  
-
   return (
     <div className="main-content">
-      <div style={heroContainerStyle}>
-        <div style={textSectionStyle}>
+      <div className="heroContainerStyle">
+        <div className="textSectionStyle">
           <div>
             <h1>Hello, I'm Brenna Clark</h1>
           </div>
           <div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
           </div>
-          <div style={buttonContainerStyle}>
-          <Link to="/about">
-              <Button variant="secondary" style={buttonStyle}>
-                  My story
+          <div className="buttonContainerStyle">
+            <Link to="/about">
+              <Button variant="secondary" className="buttonStyle">
+                My story
               </Button>
-          </Link>
-          <Link to="/contact">
-              <Button variant="primary" style={buttonStyle}>
-                  Get in touch
+            </Link>
+            <Link to="/contact">
+              <Button variant="secondary" className="buttonStyle">
+                Get in touch
               </Button>
-          </Link>
-      </div>
+            </Link>
+          </div>
         </div>
-        <div style={imageContainerStyle}>
-          <div style={profileImageStyle}></div>
-          <div style={profileCircleStyle}></div>
+        <div className="imageContainerStyle">
+          <div className="profileImageStyle"></div>
+          <div className="profileCircleStyle"></div>
         </div>
       </div>
       <Container className="my-5">
