71c7abdf3ad45af76866f5319acc5677840d5917
Brenna Clark <brennacclark@gmail.com>
2023-10-19 16:01:29 -0400

    Fixing header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-scroll";
 import "font-awesome/css/font-awesome.min.css";
 
 const Header = () => {
   const headerStyle = {
     boxShadow: "none",
+    background:
+      "linear-gradient(0deg, transparent, rgba(255, 165, 79, 0.1)), url('noise.svg')",
+    position: "fixed",
+    top: "0",
+    width: "100%",
+    zIndex: 100,
   };
 
   return (
