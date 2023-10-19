fa6f143ba32a6955231de92ec2a9a8bb9d4f01c6
Brenna Clark <brennacclark@gmail.com>
2023-10-19 15:06:38 -0400

    Make contact icons in a row above resume

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,46 +1,91 @@
-// src/pages/Contact.jsx
-import React from 'react';
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
 
 const Contact = () => {
-
   return (
     <div className="main-content">
       {/* Hero Container */}
-      <div className="heroContainer text-center">
+      <div className="heroContainer text-center" style={{marginBottom: '-5vh'}}>
         <h1>Contact Me</h1>
         <p>Let's get in touch.</p>
       </div>
 
       {/* Body Container */}
-      <div className="contactBody"  style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'flex-start'}} >
-
-          <div className="svgContainer" >
-              <img src="/BrennaClark_UX_2023.svg" alt="Resume SVG" className="responsive-svg" style={{maxWidth: '100%'}}/>
+      <div
+        className="contactBody"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {/* Contact Links */}
+        <div
+          className="contactLinkContainer"
+          style={{
+            paddingTop: "10vh",
+            marginBottom: "10vh",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <div>
+            {/* Email */}
+            <a href="mailto:brennacclark@gmail.com" title="Email">
+              <i
+                className="fa fa-envelope"
+                style={{ fontSize: "48px", margin: "0 15px" }}
+              ></i>
+            </a>
           </div>
-          
-          <div className="contactLinkContainer" style={{paddingTop: '10vh'}}>
-        
-            <div>{/* Email */}
-              <a href="mailto:brennacclark@gmail.com"><i className="material-icons">email</i> Email - brennacclark@gmail.com</a>
-            </div>
-            
-            <div> {/* LinkedIn */}
-              <a href="https://www.linkedin.com/in/brennacclark/" target="_blank" rel="noopener noreferrer"><i className="material-icons">business</i> LinkedIn - brennacclark</a>
-            </div>
-            
-            <div> {/* GitHub */}
-               <a href="https://github.com/brennacclark" target="_blank" rel="noopener noreferrer"><i className="material-icons">code</i> GitHub - brennacclark</a>
-            </div>
-
-            <div> {/* Resume Download Button */}
-              <a href="/BrennaClark_UX_2023.pdf" download style={{marginTop: '10px'}}>
-                <i className="material-icons">file_download</i> Download Resume
-              </a>
-            </div>
+          <div>
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/brennacclark/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="LinkedIn"
+            >
+              <i
+                className="fa fa-linkedin"
+                style={{ fontSize: "48px", margin: "0 15px" }}
+              ></i>
+            </a>
           </div>
+          <div>
+            {/* GitHub */}
+            <a
+              href="https://github.com/brennacclark"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="GitHub"
+            >
+              <i
+                className="fa fa-github"
+                style={{ fontSize: "48px", margin: "0 15px" }}
+              ></i>
+            </a>
+          </div>
+        </div>
+      </div>
 
-          {/* Right Column for Resume SVG */}
-
+      {/* Resume SVG */}
+      <div
+        className="svgContainer"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src="/BrennaClark_UX_2023.svg"
+          alt="Resume SVG"
+          className="responsive-svg"
+          style={{ maxWidth: "100%" }}
+        />
       </div>
     </div>
   );
