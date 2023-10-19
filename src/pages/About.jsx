366d50944eae26ac1d2424d30805a936357cd2ce
Brenna Clark <brennacclark@gmail.com>
2023-10-19 13:45:17 -0400

    Adding timeline

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import CareerTimeline from "./CareerTimeline";
 
 const About = () => {
   return (
@@ -8,34 +9,36 @@ const About = () => {
       <div className="container my-5">
         <div className="row">
           <div className="col-md-10 offset-md-1 text-center">
-            <p>
+            <p className="mb-4">
               I’m Brenna - thanks for stopping by my page. I’m a software
               developer, Rhode Islander, animal lover, gamer, and most
               importantly - human!
             </p>
-            <p>
+            <p className="mb-4">
               I find it satisfying and rewarding to create an experience that
               helps the greatest number of people. I’m curious about how modern
               tools and technology, such as AI, VR and AR, will continue to
-              integrate our everyday lives. What will the future of human-tech
-              relations look like? How can we create tools and experiences that
-              adapt to the needs of the customers, industry, and modern society?
-              And how can we continuously adapt in a responsible, ethical, and
-              equitable way?
+              integrate our everyday lives.
             </p>
-            <h2>My Skills</h2>
-            <p>
+            <p className="mb-4">
+              What will the future of human-tech relations look like? How can we
+              create tools and experiences that adapt to the needs of the
+              customers, industry, and modern society? And how can we
+              continuously adapt in a responsible, ethical, and equitable way?
+            </p>
+            <h3 className="mt-5 mb-4">My Skills</h3>
+            <p className="mb-4">
               I specialize in user experience, software quality, with 8+ years
               of experience as a software development generalist.
             </p>
-            <h2>My Journey</h2>
-            <p>
+            <h3 className="mb-4">My Journey</h3>
+            <p className="mb-4">
               I started my journey in the world of quality assurance and test
               automation. I gained experience in performance and devops shipping
               continuous software. In recent years, I've had a focus on
               technical writing and knowledge engineering.
             </p>
-            <p>
+            <p className="mb-4">
               I have always been interested in how we, as humans, interact with
               technology, the effect on its users, and society as a whole. While
               at IBM, I found that some of the most rewarding work I
@@ -46,6 +49,8 @@ const About = () => {
               enhance my existing software skills.
             </p>
           </div>
+
+          <CareerTimeline />
         </div>
       </div>
     </div>
